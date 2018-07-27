eff41797dc0ec4ae267cd3ead13ee9b1cacc6132
lnmunhoz <ln.munhoz@gmail.com>
2018-07-27 18:39:16 +0700

    feat: update message

--- projects/node-app/index.js
+++ projects/node-app/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello Lerna!'))
+app.get('/', (req, res) => res.send('Hello Lerna!!!'))
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))

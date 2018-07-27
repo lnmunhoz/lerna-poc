1959ee7b4e5284624c525293951255e3e120ffa9
lnmunhoz <ln.munhoz@gmail.com>
2018-07-27 16:21:52 +0700

    feat: create node-app

---
+++ projects/node-app/index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const commonA = require('@lnmunhoz/common-a')
+const commonB = require('@lnmunhoz/common-b')
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+
+commonA()
+commonB()
+app.listen(3000, () => console.log('Example app listening on port 3000!'))

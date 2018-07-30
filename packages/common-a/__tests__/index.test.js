b36be53031a19dd6949fd2842068bc944d5c2497
lnmunhoz <ln.munhoz@gmail.com>
2018-07-30 13:30:50 +0700

    test: fix message test

--- packages/common-a/__tests__/index.test.js
+++ packages/common-a/__tests__/index.test.js
@@ -3,6 +3,6 @@ const message = require('../message')
 
 describe('common-a tests', () => {
   it('should be the correct message', () => {
-    expect(message).toBe('Hello from module-a')
+    expect(message).toBe('Hello from module-a!')
   })
 })
